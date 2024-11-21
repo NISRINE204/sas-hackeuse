1a40a86e1ab10a3c17c560be5fddf4d6d5b7f27b
NISRINE204 <nissrinlazmi457@gmail.com>
2024-11-21 16:12:59 +0100

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,62 @@
+1   for(let i=1;i<= 10;i++){
+    console.log(i);
+}
+//exercice2
+for(let i=1;i<=5;i++){
+    console.log(i*i) 
+}
+//exercice3
+let number= 5
+while(number>=1){
+    console.log(number);
+    number--;
+}
+//exercice4
+let couleur=["rouge","vert","bleu"];
+couleur.forEach(function(couleur) {
+    console.log(couleur)
+});
+//exercice5
+for(let R=1;R<=15;R+=2){ 
+console.log(R)
+}
+// //EXERCICE6
+let produit= 1
+
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
+for(let i=0; i<=5;i++){
+    console.log(i*i)
+}
+let R=5;
+while(R>=1){
+    console.log(R);
+    R--
+}
+for(let R=5;R>=1;R--){
+    console.log(R);
+}
+let couleur=["rouge","bleu","green"]
+couleur.forEach(function(couleur){
+    console.log(couleur)
+})
+for(let i=1;i<=15;i++){
+    if(i % 3==1){
+        console.log(i)
+    }
+}
+for(let i=1;i<=5;i++){
+    console.log(i++)
+}
+let produit=1
+for(let i=1;i<=5;i++){
+      let produi +=3;
+}
+console.log("le produit de 1 à 5 est",produit)
+
+let fruit=["apple", "banana",
+"cherry"]
+fruit.forEach(function(fruit){
+    console.log(fruit.toUpperCase())
+})
